af080ddfd2360bf79656d0dd5779dfe2190b9428
Martin DAUTRICHE <dautriche.e1606532@etud.univ-ubs.fr>
2020-05-19 11:30:46 -0400

    Update :  - Change module name -> GenLabelNoiseTS

---
+++ GenLabelNoiseTS/GenerateData.py
@@ -0,0 +1,248 @@
+import random
+import numpy as np
+import pandas as pd
+
+
+class GenerateData:
+    """
+    Class GenerateData
+    The class contains only static method/function
+    """
+
+    @staticmethod
+    def generateData(seed,initFilename,classList=None):
+        """
+        :param initFilename:
+        :param seed:
+        :param class_names: contains the names of different class
+               class_names = ['Corn', 'Corn_ensilage',...]
+        :param param_val: param of double sigmo for each class
+               param_val[:14, 0] = [0.62, 0.67, 0.2, 0.25, 122, 182, 5, 20, 270, 290, 15, 20, 500, 20]
+        :param dates: number of days since New Year's Day
+               dates = [0,25,50,...]
+        :return: No return the ouput is the file.
+        """
+        # Set random_state:
+        randomState = np.random.RandomState(seed)
+        # Get class names, param, dates from initFile.
+        param_val = pd.DataFrame(pd.read_csv(initFilename))
+        if classList is None:
+            class_names = np.array(param_val['class_names'])
+        else:
+            class_names = classList
+        del param_val['class_names']
+        dates = np.array(param_val.loc[param_val.index[-1],:])
+        dates = dates[np.logical_not(np.isnan(dates))]
+        param_val = param_val.drop(param_val.index[-1])
+        param_val = np.array(param_val)
+
+        # Double or simple sigmoid
+        db_sigmo = np.where(np.sum(param_val[:, 14:], axis=1) != -len(param_val[:, 14:])+1, 26, 14)
+
+        unique_sequencePolid = GenerateData._uniqueid()
+        unique_sequencePixid = GenerateData._uniqueid()
+
+        dfHeader = []
+        tmpDataFrame = []
+        tmpProfils = []
+        # Write the header
+        # N = nbClass
+        # dates
+        # class1;nbSamples
+        # ...
+        # % classN;nbSamples
+
+        dfHeader.append(np.array(dates))
+        for i in range(0, len(class_names)):
+            dfHeader.append(np.array([class_names[i], param_val[i]]))
+        dfHeader = pd.DataFrame(np.array(dfHeader))
+
+        # Generate Data Start:
+        for add in range(0, len(class_names)):
+            data = param_val[add,:int(db_sigmo[add])]
+            nb_Samples = data[12]
+            polygonSize = data[13]
+
+            # Generate the exact number of samples per polygons
+            # (same number of samples per polygon)
+            nb_Samples_Polygons = polygonSize * np.ones((1, int(np.floor(nb_Samples / polygonSize))))
+
+            if db_sigmo[add] == 14:
+                range_param = data[:-2]
+                range_param = np.reshape(range_param, (6, 2))
+            else:
+                range_param = [*data[:12], *data[14:]]
+                range_param = np.reshape(range_param, (12, 2))
+
+            for i in range(0, len(nb_Samples_Polygons[0])):  # id poly
+                polid = next(unique_sequencePolid)
+                sigmo_param = GenerateData._generate_double_sigmo_parameters(range_param,randomState)
+                # Calculation of Gaussian parameters
+
+                # Calculation of x2
+                # Date of the inflection point of the main crop.Page 72 - 73 manuscript.
+                if db_sigmo[add] == 14:
+                    x2 = sigmo_param[0, 4]
+                else:
+                    x2 = sigmo_param[0, 10]
+
+                # Average dates of regrowth. The regrowth curve is offset in relation to the main crop (for
+                # winter crops the regrowth is in summer and vice versa for winter crops).
+                mu_gauss = randomState.randint(50, 151) + x2
+
+                # Standard deviation of regrowth
+                sigma_gauss = 16 * randomState.rand() + 32
+
+                # Amplitude of regrowth
+                A_gauss = randomState.rand() / 3
+
+                if GenerateData._strcmp(class_names[add], 'Evergreen') | GenerateData._strcmp(
+                        class_names[add], 'Decideous'):
+                    A_gauss = 0
+
+                # Matlab code for gauss
+                # gauss = A_gauss.*exp(-(dates-mean(dates)).*(dates-mean(dates))./(2.*sigma_gauss.*sigma_gauss));
+                gauss = A_gauss * np.exp(
+                    -(dates - np.mean(dates)) * (dates - np.mean(dates)) / (2 * sigma_gauss * sigma_gauss))
+
+                pos_mean = np.where(dates <= np.mean(dates))
+                pos_x2 = np.where(dates <= mu_gauss)
+                diff_pos = pos_x2[0][-1] - pos_mean[0][-1]
+                if diff_pos < 0:
+                    diff_pos = pos_mean[0][-1] - pos_x2[0][-1]
+
+                for j in range(0, int(nb_Samples_Polygons[0, i])):  # pixel poly
+                    # Generate variability : 3eme version
+
+                    reduce_sigmo_param = np.zeros((len(sigmo_param[0]), 2))
+                    variability = randomState.randint(5, 21)
+                    reduce_sigmo_param[:, 0] = sigmo_param - (range_param[:, 1] - range_param[:, 0]) / variability
+                    reduce_sigmo_param[:, 1] = sigmo_param + (range_param[:, 1] - range_param[:, 0]) / variability
+
+                    reduce_sigmo_param[:, 0] = np.where(reduce_sigmo_param[:, 0] < range_param[:, 0], range_param[:, 0],
+                                                        reduce_sigmo_param[:, 0])
+                    reduce_sigmo_param[:, 1] = np.where(reduce_sigmo_param[:, 1] > range_param[:, 1], range_param[:, 1],
+                                                        reduce_sigmo_param[:, 1])
+
+                    sample_sigmo_param = GenerateData._generate_double_sigmo_parameters(reduce_sigmo_param,randomState)
+
+                    if db_sigmo[add] == 14:
+                        init_profil = GenerateData._sigmoProfil(sample_sigmo_param, dates)
+                    else:
+                        init_profil = GenerateData._doubleSigmoProfil(sample_sigmo_param, dates)
+
+                    norm_pdf = 0.05 * (2 * randomState.rand(1, len(dates)))
+                    vec_noisy_dates = randomState.permutation(randomState.randint(0, len(dates), size=len(dates)))
+                    norm_pdf[0, vec_noisy_dates[:randomState.randint(0, len(dates))]] = 0
+                    profil = norm_pdf + init_profil
+
+                    if len(dates) - diff_pos > diff_pos:
+                        # Pour ajouter des tab à la suite: gauss2 = [*l1,*l2,*l3]
+                        gauss2 = np.array([*gauss[(len(dates) - diff_pos):], *gauss[:diff_pos],
+                                           *gauss[diff_pos:len(dates) - diff_pos]]).reshape(1, 15)
+                        profil[0] = profil[0] + gauss2
+                    else:
+                        # Origin code Matlab: profil = profil + [gauss(length(dates)-diff_pos+1:diff_pos) gauss(
+                        # diff_pos+1:end) gauss(1:length(dates)-diff_pos)]
+                        gauss2 = np.array([*gauss[(len(dates) - diff_pos):diff_pos], *gauss[diff_pos:],
+                                           *gauss[:len(dates) - diff_pos]]).reshape(1, 15)
+                        profil[0] = profil[0] + gauss2
+
+                    # Return max between -np.ones((1, np.size(profil))) and profil
+                    profil = np.where(-np.ones((1, np.size(profil))) > profil, -np.ones((1, np.size(profil))), profil)
+                    # Return min between np.ones((1, np.size(profil))) and profil
+                    profil = np.where(np.ones((1, np.size(profil))) < profil, np.ones((1, np.size(profil))), profil)
+                    tmpDataFrame.append([class_names[add], polid, next(unique_sequencePixid)])
+                    tmpProfils.append(profil[0])
+
+        tmpDataFrame = np.array(tmpDataFrame)
+        tmpProfils = np.array(tmpProfils)
+        tmplen = len(tmpProfils[0])
+        tmpProfils = np.transpose(tmpProfils)
+        dfData = pd.DataFrame(tmpDataFrame, columns=['label', 'polid', 'pixid', ])
+        for i in range(tmplen):
+            dfData['d' + str(i + 1)] = tmpProfils[i]
+        return dfHeader, dfData
+
+    #  -----------------------------------------------------------------------------------------------------------------
+    #  Intern (Private) Functions of this class.
+    #  For normal use, don't use them.
+    #  -----------------------------------------------------------------------------------------------------------------
+
+    @staticmethod
+    def _strcmp(str1, str2):
+        """
+        Function to compare two string with each other
+        :param str1: first string
+        :param str2: second string
+        :return: true if and only if the both string are the same
+        """
+        if str1 == str2:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def _sigmoProfil(samples_sigmo_param, dates):
+        """
+
+        :param samples_sigmo_param:
+        :param dates: number of days since New Year's Day
+               dates = [0,25,50,...]
+        :return: a sigmo profil
+        """
+        # Matlab code
+        # profil =  samples_sigmo_param(1) .* ...
+        #         ( 1./ ( 1 + exp((samples_sigmo_param(3)-dates)./samples_sigmo_param(4)) )...
+        #         - 1./ ( 1 + exp((samples_sigmo_param(5)-dates)./samples_sigmo_param(6)) ) )...
+        #         + samples_sigmo_param(2);
+        profil = samples_sigmo_param[0, 0] * (
+                1 / (1 + np.exp((samples_sigmo_param[0, 2] - dates) / samples_sigmo_param[0, 3])) - 1 / (
+                1 + np.exp((samples_sigmo_param[0, 4] - dates) / samples_sigmo_param[0, 5]))) + (samples_sigmo_param[
+                                                                                                     0, 1] - 0.05)
+        return profil
+
+    @staticmethod
+    def _doubleSigmoProfil(samples_sigmo_param, dates):
+        """
+
+        :param samples_sigmo_param: [A ; B ; x0 ; x1 ; x2 ; x3]
+        :param dates: number of days since New Year's Day
+               dates = [0,25,50,...]
+        :return: a double sigmo profil
+        """
+        profil1 = GenerateData._sigmoProfil(samples_sigmo_param[:, :6], dates)
+        profil2 = GenerateData._sigmoProfil(samples_sigmo_param[:, 6:], dates) + 0.05
+        return profil1 + profil2
+
+    @staticmethod
+    def _generate_double_sigmo_parameters(range_param,randomState):
+        """
+
+        :param range_param: contains min and max value for the six params
+               of the double sigmoid [A ; B ; x0 ; x1 ; x2 ; x3]
+        :return: give the six double_sigmoid parameters
+                [A ; B ; x0 ; x1 ; x2 ; x3]
+        """
+        sigmo_param = np.zeros((1, len(range_param)))
+        for i in range(0, len(range_param)):
+            if range_param[i, 0] > range_param[i, 1]:
+                print('Error MIN > MAX')
+            mu = (range_param[i, 0] + range_param[i, 1]) / 2.0
+            sqrt_var = (range_param[i, 1] - mu) / 3.0
+            val = sqrt_var * randomState.randn() + mu
+            while val < range_param[i, 0] or val > range_param[i, 1]:
+                val = sqrt_var * randomState.randn() + mu
+            sigmo_param[0, i] = val
+        return sigmo_param
+
+    @staticmethod
+    def _uniqueid():
+        """
+
+        :return: Generate an generator for unique ID
+        """
+        seed = random.getrandbits(16)
+        while True:
+            yield seed
+            seed += 1
---
+++ GenLabelNoiseTS/GeneratorData.py
@@ -0,0 +1,310 @@
+import h5py
+from GenLabelNoiseTS.DrawProfils import *
+from GenLabelNoiseTS.GenerateData import *
+from GenLabelNoiseTS.GeneratorNoise import *
+from GenLabelNoiseTS.WriteGenerateData import *
+import numpy as np
+
+
+class GeneratorData:
+
+    def __init__(self, filename, rep='', classList=None, pathInitFile=None, seedData=None, csv=False, verbose=False):
+        """
+
+        :param filename: Name of the file h5
+        """
+        # Path to initFilename
+        if pathInitFile is None:
+            self._initFilename = '../initFile.csv'
+        else:
+            self._initFilename = pathInitFile
+
+        self._classList = classList
+
+        if seedData is None:
+            seedData = np.random.randint(100000)
+
+        self._verbose = verbose
+        if self._verbose is True:
+            print("Init Start !")
+
+        self._rep = rep
+        self._filename = filename
+        self._dfTest = None
+        self._csv = csv
+        self._seedData = seedData
+        # Test if not file exist
+        if not os.path.isfile(self._rep + self._filename):
+            (dfHeader, dfData) = self._genData()
+            self._dfHeader = dfHeader
+            self._dfData = dfData
+            WriteGenerateData.writeGenerateDataToH5(self._filename, self._rep, self._dfHeader, self._dfData, self._csv)
+            if self._verbose is True:
+                print("Generate Data Done !")
+        else:
+            # File exist
+            self._dfHeader = pd.DataFrame(pd.read_hdf(self._rep + self._filename, 'header'))
+            # Test if convert is needed
+            if csv is False:
+                try:
+                    dfCsv = pd.read_hdf(self._rep + self._filename, 'csvFile')
+                    npCsv = np.array(pd.DataFrame(dfCsv))
+                    npCsv = npCsv.reshape((len(npCsv),))
+                    self._dfData = pd.DataFrame(pd.read_csv(npCsv[0]))
+                    self._convertCsvToh5(npCsv)
+                except KeyError:
+                    self._dfData = pd.DataFrame(pd.read_hdf(self._rep + self._filename, 'data'))
+            else:
+                try:
+                    self._dfData = pd.DataFrame(pd.read_hdf(self._rep + self._filename, 'data'))
+                    self._converth5ToCsv()
+                except KeyError:
+                    dfCsv = pd.read_hdf(self._rep + self._filename, 'csvFile')
+                    npCsv = np.array(pd.DataFrame(dfCsv))
+                    npCsv = npCsv.reshape((len(npCsv),))
+                    self._dfData = pd.DataFrame(pd.read_csv(npCsv[0]))
+            if self._verbose is True:
+                print("The file already exists !!!")
+        if self._verbose is True:
+            print("Init Done !")
+
+    def getDataXY(self):
+        """
+        Public Fonction
+        :return: X and Y. X is a matrix containing profils NDVI generate. Y is label matrix
+        """
+        return self._generateXY()
+
+    def getNoiseDataXY(self, noiseLevel, dictClassSystematicChange=None, seedNoise=None):
+        """
+        Public Fonction
+        :param noiseLevel: Level of noise in %, format like that 0.05
+        :param dictClassSystematicChange: Dictionary format like that {'Wheat': 'Barley', 'Barley': 'Soy'}
+        :return: X and Y. X is a matrix containing profils NDVI generate. Y is label matrix with label noise
+        """
+        if seedNoise is None:
+            seedNoise = np.random.randint(100000)
+        dfNoise = self._generateNoise(noiseLevel, dictClassSystematicChange, seedNoise)
+        return self._generateXY(dfNoise)
+
+    def getTestData(self):
+        """
+        Public Fonction
+        :return: X and Y. X is a new matrix test containing profils NDVI generate. Y is label matrix
+        """
+        dfTest = self._generateTest()
+        tmpdfData = self._dfData
+        self._dfTest = dfTest
+        self._dfData = dfTest
+        (X, Y) = self._generateXY()
+        self._dfData = tmpdfData
+        return X, Y
+
+    #  -----------------------------------------------------------------------------------------------------------------
+    #  Intern (Private) Functions of this class.
+    #  For normal use, don't use them.
+    #  -----------------------------------------------------------------------------------------------------------------
+    def _generateXY(self, dfLabel=None):
+        """
+        Intern Fonction
+        :param dfLabel: DataFrame containing name of class for each pixel profil
+        :return: X and Y. X is a new matrix test containing profils NDVI generate. Y is label matrix
+        """
+        if dfLabel is None:
+            dfLabel = self._dfData
+        Y = np.array(dfLabel.sort_values(by=['pixid'])['label']).reshape(
+            len(np.array(dfLabel.sort_values(by=['pixid'])['label'])), 1)
+        X = np.array(self._dfData.sort_values(by=['pixid']).loc[:, 'd1':])
+        X1 = np.ones((len(X), len(np.array(self._dfHeader.loc[0, :])[0])))
+        for i in range(len(X)):
+            X1[i] = X[i]
+        return X1, Y
+
+    def _generateNoise(self, noiseLevel, dictClassSystematicChange, seedNoise):
+        """
+        Intern Fonction
+        :param noiseLevel: Level of noise in %, format like that 0.05
+        :param dictClassSystematicChange: Dictionary format like that {'Wheat': 'Barley', 'Barley': 'Soy'}
+        :return: DataFrame like this columns=['pixid', 'noisy', 'label']
+        """
+        name = self._genName(dictClassSystematicChange, noiseLevel)
+        if not ReadGenerateData.getAlreadyGenNoise(self._filename, self._rep, name, self._csv):
+            (noiseLevel, dfNoise, systematicChange) = GeneratorNoise.generatorNoisePerClass(self._filename, self._rep,
+                                                                                            noiseLevel, seedNoise,
+                                                                                            dictClassSystematicChange,
+                                                                                            self._csv)
+            WriteGenerateData.writeGenerateNoisyData(self._filename, self._rep, noiseLevel, dfNoise, systematicChange,
+                                                     self._csv, dictClassSystematicChange)
+            if self._verbose is True:
+                print("Generate Noise Done !")
+        else:
+            dfNoise = ReadGenerateData.getByNameNoise(self._filename, self._rep, name, self._csv)
+            if self._verbose is True:
+                print("Generate Noise Already Done !")
+        return dfNoise
+
+    def _generateTest(self):
+        if self._csv is False:
+            try:
+                dfTest = pd.DataFrame(pd.read_hdf(self._rep + self._filename, 'test'))
+                if self._verbose is True:
+                    print('Generate Test already Done !')
+            except KeyError:
+                (dfHeader, dfTest) = self._genData()
+                WriteGenerateData.writeTest(self._filename, self._rep, dfTest, self._csv)
+                if self._verbose is True:
+                    print('Generate Test Done !')
+        else:
+            try:
+                dfTest = pd.DataFrame(pd.read_csv(self._rep + 'test.csv'))
+                if self._verbose is True:
+                    print('Generate Test already Done !')
+            except FileNotFoundError:
+                (dfHeader, dfTest) = self._genData()
+                WriteGenerateData.writeTest(self._filename, self._rep, dfTest, self._csv)
+                if self._verbose is True:
+                    print('Generate Test Done !')
+        return dfTest
+
+    def _genData(self):
+        """
+        Intern Fonction
+        :return: 2 DataFrame Header and Data
+        """
+        (dfHeader, dfData) = GenerateData.generateData(seed=self._seedData, initFilename=self._initFilename,
+                                                       classList=self._classList)
+        return dfHeader, dfData
+
+    def _convertCsvToh5(self, npCsv):
+        if self._verbose is True:
+            print('Convert Csv To h5 start...')
+        # Test if dataset Test exist
+        try:
+            dfTest = pd.DataFrame(pd.read_csv(self._rep + 'test.csv'))
+        except FileNotFoundError:
+            dfTest = None
+        # Save old path
+        tmpFileName = self._filename
+        # Remove path of test.csv
+        npCsv = npCsv[npCsv != self._rep + 'test.csv']
+        self._filename = "test.h5"
+        # Write dataset Data
+        WriteGenerateData.writeGenerateDataToH5(self._filename, self._rep, self._dfHeader, self._dfData, self._csv)
+        # If dataset Test exist
+        if dfTest is not None:
+            # Write dataset Test and remove csv file
+            WriteGenerateData.writeTest(self._filename, self._rep, dfTest, self._csv)
+            os.remove(self._rep + 'test.csv')
+        # Get content of LUT
+        npLutCsv = np.array(pd.DataFrame(pd.read_csv(self._rep + 'lut.csv'))['dict'])
+        # String to dict
+        npLutCsv = [eval(i) for i in npLutCsv]
+        # Remove path of data.csv
+        npCsv = npCsv[npCsv != self._rep + 'data.csv']
+        # Write noisy dataset
+        for i in range(len(npCsv)):
+            systematicChange = npCsv[i].split("/")[-1].split(".")[0]
+            # Get noisy dataset
+            dfNoise = pd.read_csv(npCsv[i])
+            tmpstring = systematicChange.split('_')
+            # Get noise level
+            noiseLevel = int(tmpstring[1]) / 100
+            if len(npLutCsv) == 0:
+                WriteGenerateData.writeGenerateNoisyData(self._filename, self._rep, noiseLevel, dfNoise,
+                                                         systematicChange,
+                                                         self._csv, None)
+            else:
+                WriteGenerateData.writeGenerateNoisyData(self._filename, self._rep, noiseLevel, dfNoise,
+                                                         systematicChange, self._csv, npLutCsv[i - 1])
+
+            os.remove(self._rep + systematicChange + '.csv')
+        os.remove(self._rep + 'data.csv')
+        os.remove(self._rep + 'lut.csv')
+        os.remove(self._rep + tmpFileName)
+        os.rename(self._rep + self._filename, self._rep + tmpFileName)
+        self._filename = tmpFileName
+        if self._verbose is True:
+            print('Convert Csv To h5 done !')
+
+    def _converth5ToCsv(self):
+        if self._verbose is True:
+            print('Convert h5 To Csv start...')
+        # Get name of dataset
+        file = h5py.File(self._rep + self._filename, 'r')
+        tmptab = list(file.keys())
+        file.close()
+        tmpDfNoise = []
+        # Test if dataset Test exist
+        try:
+            dfTest = pd.read_hdf(self._rep + self._filename, tmptab[tmptab.index('test')])
+            del tmptab[tmptab.index('test')]
+        except ValueError:
+            dfTest = None
+        # Get dataset by name
+        self._dfData = pd.read_hdf(self._rep + self._filename, tmptab[tmptab.index('data')])
+        self._dfHeader = pd.read_hdf(self._rep + self._filename, tmptab[tmptab.index('header')])
+        tmpDfHeaderNoise = np.array(
+            pd.DataFrame(pd.read_hdf(self._rep + self._filename, tmptab[tmptab.index('headerNoise')])))
+        # Reshape to horizontal array
+        tmpDfHeaderNoise = tmpDfHeaderNoise.reshape((len(tmpDfHeaderNoise),))
+        npLutCsv = np.array(pd.DataFrame(pd.read_hdf(self._rep + self._filename, 'lut'))['dict'])
+        # Remove already processed datasets
+        del tmptab[tmptab.index('data')]
+        del tmptab[tmptab.index('header')]
+        del tmptab[tmptab.index('headerNoise')]
+        del tmptab[tmptab.index('lut')]
+        # Get noisy Dataframe
+        for i in tmptab:
+            tmpDfNoise.append(pd.read_hdf(self._rep + self._filename, i))
+        tmpFileName = self._filename
+
+        # Save old path
+        self._filename = "test.h5"
+        # Write dataset Data
+        WriteGenerateData.writeGenerateDataToH5(self._filename, self._rep, self._dfHeader, self._dfData, self._csv)
+        # If dataset Test exist
+        if dfTest is not None:
+            # Write dataset Test and remove csv file
+            WriteGenerateData.writeTest(self._filename, self._rep, dfTest, self._csv)
+        # String to dict
+        npLutCsv = [eval(i) for i in npLutCsv]
+        # Write noisy dataset
+        for i, j, k in zip(tmpDfHeaderNoise, tmpDfNoise, npLutCsv):
+            systematicChange = i.split(".")[0]
+            tmpstring = systematicChange.split('_')
+            # Get noisy dataset
+            dfNoise = j
+            # Get noise level
+            noiseLevel = int(tmpstring[1]) / 100
+            WriteGenerateData.writeGenerateNoisyData(self._filename, self._rep, noiseLevel, dfNoise, systematicChange,
+                                                     self._csv, k)
+        os.remove(self._rep + tmpFileName)
+        os.rename(self._rep + self._filename, self._rep + tmpFileName)
+        self._filename = tmpFileName
+        if self._verbose is True:
+            print('Convert h5 To Csv done !')
+
+    @staticmethod
+    def _genName(dictClass, noiseLevel):
+        """
+        Intern Fonction
+        :param dictClass: dictClassSystematicChange: Dictionary format like that {'Wheat': 'Barley', 'Barley': 'Soy'}
+        :param noiseLevel: Level of noise in %, format like that 0.05
+        :return: Name of the dataset in file h5
+        """
+        name = ''
+        if dictClass is not None:
+            name = 'systematic_' + str(int(noiseLevel * 100)) + '_' + str(
+                int(hashlib.md5(str(dictClass).encode("UTF-8")).hexdigest(), 16))
+        else:
+            name = 'random_' + str(int(noiseLevel * 100))
+        return name
+
+    def visualisation(self, rep):
+        nbClass = len(self._dfHeader) - 1
+        classNames = []
+        for i in range(1, nbClass + 1):
+            classNames.append(self._dfHeader[0][i][0])
+        for i in classNames:
+            Drawprofils.drawProfilClass(i, dfHeader=self._dfHeader, dfData=self._dfData, vis=True, rep=rep)
+        Drawprofils.drawProfilMeanClass(dfHeader=self._dfHeader, dfData=self._dfData, vis=True, rep=rep)
