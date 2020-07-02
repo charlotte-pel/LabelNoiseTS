0323f6cb72badc578798a9b5d9257ca7107d5f80
Martin DAUTRICHE <dautriche.e1606532@etud.univ-ubs.fr>
2020-07-02 10:47:50 +0200

    Update :  - Update main.py  - Add new file: CheckNoiseFunc.py

---
+++ GenLabelNoiseTS/CheckNoiseFunc.py
@@ -0,0 +1,60 @@
+from GenLabelNoiseTS.GenLabelNoiseTS import *
+from multiprocessing import Pool
+
+
+def getNoiseMatrixForSpeRun(numRun, path):
+    noiseArray = [round(i, 2) for i in np.arange(0, 1.05, 0.05)]
+    generator = GenLabelNoiseTS(filename="dataFrame.h5", rep=path + 'Run' + str(numRun) + '/', csv=True,
+                                verbose=False)
+    (Xtrue, ytrue) = generator.getDataXY()
+    for i in noiseArray:
+        print('Run ' + str(numRun) + ' pour ' + str(i * 100) + '%')
+        (Xtrain, ytrain) = generator.getNoiseDataXY(i)
+        print(generator.getNoiseMatrix(ytrue, ytrain))
+
+
+def checkNoiseForTwoFiveTenClass(verbose=False):
+    print('Check Starting....')
+    with Pool(6) as p:
+        p.starmap(checkGeneratingNoise,
+                  [(500, 2, './data/TwoClass/', verbose),
+                   (500, 5, './data/FiveClass/', verbose),
+                   (500, 10, './data/TenClass/', verbose)])
+    print('Check ending !!!')
+
+
+def checkGeneratingNoise(nbSamples, nbClass, path, verbose=False):
+    noiseArray = [round(i, 2) for i in np.arange(0, 1.05, 0.05)]
+    nbFirstRun = 1
+    nbLastRun = 10
+    indexRunList = []
+    nbSamplesAllClass = nbClass * nbSamples
+    for i in range(nbFirstRun, nbLastRun + 1):
+        results = []
+        indexRunList.append('Run' + str(i))
+        for j in noiseArray:
+
+            if verbose is True:
+                print(
+                    '------------------------------------------------------------------------------------------------')
+                print('nbClass = ' + str(nbClass))
+                print('NoiseLevel = ' + str(j))
+
+            generator = GenLabelNoiseTS(filename="dataFrame.h5", rep=path + 'Run' + str(i) + '/', csv=True,
+                                        verbose=False)
+            (Xtrain, ytrain) = generator.getNoiseDataXY(j)
+            (Xtrue, ytrue) = generator.getDataXY()
+
+            if verbose is True:
+                print(
+                    True if (np.sum(np.diag(generator.getNoiseMatrix(ytrue, ytrain))) == round(
+                        nbSamplesAllClass * (1 - j))) else False)
+
+            assert (np.sum(np.diag(generator.getNoiseMatrix(ytrue, ytrain))) == round(
+                nbSamplesAllClass * (1 - j))), "\nError_Generating_Noise\n" + "nbClass =" + str(
+                nbClass) + "Run = " + str(
+                i) + "\n" + "Noise Level = " + str(j)
+
+            if verbose is True:
+                print(
+                    '------------------------------------------------------------------------------------------------')
--- main.py
+++ main.py
@@ -1,77 +1,28 @@
-from multiprocessing import Pool
-
 from EvalAlgo.EvalAlgo import *
-from pathVar import *
+from GenLabelNoiseTS.GenLabelNoiseTS import *
+import numpy as np
 
-# from CNNTemp.train_classifier import *
-
+pathTwoClass = './data/TwoClass/'
+pathFiveClass = './data/FiveClass/'
+pathTenClass = './data/TenClass/'
 
 def main():
     systematicChange = False
     nbclass = 2
     seed = 0
 
-    EvalAlgo(pathTwoClass, nbclass, seed, systematicChange)
-    visualisationEval(pathTwoClass, 'Two classes', systematicChange)
+    # EvalAlgo(pathTwoClass, nbclass, seed, systematicChange)
+    visualisationEval(2, None, False)
+    visualisationEval(5, None, False)
+    visualisationEval(5, None, True)
+    visualisationEval(10, None, False)
 
-
-def getNoiseMatrixForSpeRun(numRun, path):
-    noiseArray = [round(i, 2) for i in np.arange(0, 1.05, 0.05)]
-    generator = GenLabelNoiseTS(filename="dataFrame.h5", rep=path + 'Run' + str(numRun) + '/', csv=True,
-                                verbose=False)
-    (Xtrue, ytrue) = generator.getDataXY()
-    for i in noiseArray:
-        print('Run ' + str(numRun) + ' pour ' + str(i * 100) + '%')
-        (Xtrain, ytrain) = generator.getNoiseDataXY(i)
-        print(generator.getNoiseMatrix(ytrue, ytrain))
-
-
-def checkNoiseForTwoFiveTenClass(verbose=False):
-    print('Check Starting....')
-    with Pool(6) as p:
-        p.starmap(checkGeneratingNoise,
-                  [(500, 2, 'TwoClass/', verbose),
-                   (500, 5, 'FiveClass/', verbose),
-                   (500, 10, 'TenClass/', verbose)])
-    print('Check ending !!!')
-
-
-def checkGeneratingNoise(nbSamples, nbClass, pathClass, verbose=False):
-    pathClass = rootPath + pathClass
-    noiseArray = [round(i, 2) for i in np.arange(0, 1.05, 0.05)]
-    nbFirstRun = 1
-    nbLastRun = 10
-    indexRunList = []
-    nbSamplesAllClass = nbClass * nbSamples
-    for i in range(nbFirstRun, nbLastRun + 1):
-        results = []
-        indexRunList.append('Run' + str(i))
-        for j in noiseArray:
-
-            if verbose is True:
-                print(
-                    '------------------------------------------------------------------------------------------------')
-                print('nbClass = ' + str(nbClass))
-                print('NoiseLevel = ' + str(j))
-
-            generator = GenLabelNoiseTS(filename="dataFrame.h5", rep=pathClass + 'Run' + str(i) + '/', csv=True,
-                                        verbose=False)
-            (Xtrain, ytrain) = generator.getNoiseDataXY(j)
-            (Xtrue, ytrue) = generator.getDataXY()
-
-            if verbose is True:
-                print(
-                    True if (np.sum(np.diag(generator.getNoiseMatrix(ytrue, ytrain))) == round(
-                        nbSamplesAllClass * (1 - j))) else False)
-
-            assert (np.sum(np.diag(generator.getNoiseMatrix(ytrue, ytrain))) == round(
-                nbSamplesAllClass * (1 - j))), "\nError_Generating_Noise\n" + "nbClass =" + str(
-                nbClass) + "Run = " + str(
-                i) + "\n" + "Noise Level = " + str(j)
-
-            if verbose is True:
-                print(
-                    '------------------------------------------------------------------------------------------------')
+    # classList = ('Corn', 'Corn_Ensilage', 'Sorghum', 'Sunflower', 'Soy',
+    #              'Wheat', 'Rapeseed', 'Barley',
+    #              'Evergreen', 'Decideous')
+    # generator = GenLabelNoiseTS(filename="dataFrame.h5", rep='src/file/', classList=classList, csv=True,
+    #                             verbose=True)
+    # generator.visuTest(typePlot='randomPoly', className='Corn')
 
 
 if __name__ == "__main__": main()
